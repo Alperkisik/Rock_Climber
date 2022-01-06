d7df27ae56384f1b4611d909786067dc55635d18
Alperkisik <60811707+Alperkisik@users.noreply.github.com>
2022-01-06 17:16:48 +0300

    commit

--- Assets/Scripts/Level/InputManager.cs
+++ Assets/Scripts/Level/InputManager.cs
@@ -13,6 +13,21 @@ public class InputManager : MonoBehaviour
 
     private void Tab()
     {
+        //touch input
+        if (Input.touchCount > 0)
+        {
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.GetTouch(0).position), out hitInfo);
+            if (hit)
+            {
+                if (hitInfo.transform.gameObject.tag == "Rock")
+                {
+                    LevelManager.instance.Event_Tab(hitInfo.transform.gameObject);
+                }
+            }
+        }
+
+        //mouse input
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hitInfo = new RaycastHit();
